10fb20e5fa07a994f9e4d589948e9cf9e804d5b9
Marc Simmen <marc.simmen@swisssoftware.group>
2023-07-26 08:07:22 +0000

    bugfix: keep newest instead of oldest logfiles

--- pilatus-rt/src/tracing/logfile_writer.rs
+++ pilatus-rt/src/tracing/logfile_writer.rs
@@ -34,7 +34,7 @@ impl io::Write for LogFileWriter<RollingFileAppender> {
                     let n = f.path();
                     io::Result::Ok((t, n))
                 }),
-                |i| i.sorted_by_key(|x| x.0).skip(self.files_to_keep),
+                |i| i.sorted_by_key(|x| x.0).rev().skip(self.files_to_keep),
             )?;
 
             for (_, p) in files {
@@ -44,5 +44,44 @@ impl io::Write for LogFileWriter<RollingFileAppender> {
         }
 
         self.inner.flush()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use futures::{pin_mut, StreamExt};
+    use pilatus::visit_directory_files;
+    use std::{io::Write, time::Duration};
+    use tokio::fs::File;
+
+    #[tokio::test]
+    async fn keep_newest_files() {
+        let tmpdir = tempfile::tempdir().unwrap();
+        let tmppath = tmpdir.into_path();
+
+        File::create(tmppath.join("file1.log")).await.unwrap();
+        tokio::time::sleep(Duration::from_millis(1)).await;
+        File::create(tmppath.join("file2.log")).await.unwrap();
+        tokio::time::sleep(Duration::from_millis(1)).await;
+        File::create(tmppath.join("file3.log")).await.unwrap();
+        tokio::time::sleep(Duration::from_millis(1)).await;
+
+        let mut logfilewriter = LogFileWriter::new(
+            tracing_appender::rolling::never(&tmppath, "pilatus-logs"),
+            &tmppath,
+            2,
+        );
+
+        logfilewriter.flush().unwrap();
+
+        let files = visit_directory_files(tmppath);
+        pin_mut!(files);
+
+        #[rustfmt::skip]
+        assert_eq!("file3.log",files.next().await.unwrap().unwrap().file_name().to_str().unwrap());
+        #[rustfmt::skip]
+        assert_eq!("pilatus-logs",files.next().await.unwrap().unwrap().file_name().to_str().unwrap());
+        assert!(files.next().await.is_none());
     }
 }
