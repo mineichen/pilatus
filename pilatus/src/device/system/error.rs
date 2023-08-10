9b0c7ca9c6b79bdb3567d8826b65aa0098668fb8
Markus Ineichen <markus.ineichen@lipautomation.com>
2023-08-10 09:06:28 +0000

    Broadcast-Subscription fails if WeakSender::tell() doesn't accept new messages

--- pilatus/src/device/system/error.rs
+++ pilatus/src/device/system/error.rs
@@ -27,15 +27,6 @@ pub enum ActorError<TCustom: Debug> {
     #[error("Running into timeout when handling request")]
     Timeout,
 }
-#[derive(Debug, thiserror::Error, PartialEq, Eq)]
-#[non_exhaustive]
-pub enum ActorWeakTellError {
-    #[error("{0}")]
-    UnknownDevice(#[from] ActorErrorUnknownDevice),
-
-    #[error("Too much load on the system: {0}")]
-    Busy(#[from] ActorErrorBusy),
-}
 
 impl<T: Debug> From<Aborted> for ActorError<T> {
     fn from(_: Aborted) -> Self {
@@ -95,6 +86,25 @@ impl<TCustom: Debug> ActorError<TCustom> {
     pub fn custom(custom: impl Into<TCustom>) -> Self {
         Self::Custom(custom.into())
     }
+}
+
+impl<T: Debug> From<ActorWeakTellError> for ActorError<T> {
+    fn from(value: ActorWeakTellError) -> Self {
+        match value {
+            ActorWeakTellError::UnknownDevice(x) => x.into(),
+            ActorWeakTellError::Busy(x) => x.into(),
+        }
+    }
+}
+
+#[derive(Debug, thiserror::Error, PartialEq, Eq)]
+#[non_exhaustive]
+pub enum ActorWeakTellError {
+    #[error("{0}")]
+    UnknownDevice(#[from] ActorErrorUnknownDevice),
+
+    #[error("Too much load on the system: {0}")]
+    Busy(#[from] ActorErrorBusy),
 }
 
 pub trait ActorErrorResultExtensions<T, TErr: Debug> {
