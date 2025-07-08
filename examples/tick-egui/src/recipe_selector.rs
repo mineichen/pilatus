dea82b6ae581439df4d9744dcd2d02b89f0336b5
Markus Ineichen <kusi.ineichen@gmail.com>
2025-07-08 14:04:41 +0200

    Add egui recipe editor, update dependencies

---
+++ examples/tick-egui/src/recipe_selector.rs
@@ -0,0 +1,25 @@
+use pilatus::Recipes;
+
+pub(super) struct RecipeSelector {
+    base_url: String,
+    recipes: Option<Recipes>,
+}
+
+impl RecipeSelector {
+    pub fn from_base(base_url: String) -> Self {
+        Self {
+            base_url,
+            recipes: None,
+        }
+    }
+    pub(super) fn select_ui(&mut self, ui: &mut egui::Ui) -> Option<Recipes> {
+        let list_request = ehttp::Request::get(&self.base_url);
+        ehttp::fetch(
+            list_request,
+            move |result: ehttp::Result<ehttp::Response>| {
+                println!("Status code: {:?}", result.unwrap().status);
+            },
+        );
+        None
+    }
+}
