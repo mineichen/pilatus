08db7d4cf58e0368b1ddd8a599dccdde679add3d
Markus Ineichen <kusi.ineichen@gmail.com>
2025-11-16 15:51:27 +0100

    Flatten hierarchy

--- examples/tick/src/lib.rs
+++ examples/tick/src/lib.rs
@@ -1,12 +1,45 @@
-mod greeter;
-mod manual_tick;
-mod timer_tick;
+use serde::{Deserialize, Serialize};
+use std::num::NonZeroU64;
 
-#[cfg(feature = "unstable")]
-pub use greeter::Language as GreeterLanguage;
-#[cfg(feature = "unstable")]
-pub use greeter::Params as GreeterParams;
-#[cfg(feature = "unstable")]
-pub use manual_tick::Params as ManualTickParams;
-#[cfg(feature = "unstable")]
-pub use timer_tick::Params as TimerTickParams;
+pilatus::unstable_pub!(
+    #[derive(Debug, Default, Deserialize, Serialize, impex::Impex)]
+    #[serde(deny_unknown_fields)]
+    struct GreeterParams {
+        pub lang: GreeterLanguage,
+    }
+);
+
+pilatus::unstable_pub!(
+    #[derive(Debug, Default, Deserialize, Serialize, Clone)]
+    enum GreeterLanguage {
+        #[default]
+        English,
+        German,
+    }
+);
+
+impl impex::ImpexPrimitive for GreeterLanguage {}
+
+pilatus::unstable_pub!(
+    #[derive(Serialize, Deserialize, Default, Debug, impex::Impex)]
+    #[serde(default)]
+    struct ManualTickParams {
+        pub initial_count: u32,
+    }
+);
+
+pilatus::unstable_pub!(
+    #[derive(Debug, Clone, Deserialize, Serialize, impex::Impex)]
+    #[serde(deny_unknown_fields)]
+    struct TimerTickParams {
+        pub milli_seconds_per_step: NonZeroU64,
+    }
+);
+
+impl Default for TimerTickParams {
+    fn default() -> Self {
+        Self {
+            milli_seconds_per_step: const { NonZeroU64::new(100).unwrap() },
+        }
+    }
+}
--- examples/tick/src/manual_tick.rs
+++
@@ -1,9 +0,0 @@
-use serde::{Deserialize, Serialize};
-
-pilatus::unstable_pub!(
-    #[derive(Serialize, Deserialize, Default, Debug, impex::Impex)]
-    #[serde(default)]
-    struct Params {
-        pub initial_count: u32,
-    }
-);
