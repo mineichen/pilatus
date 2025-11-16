3b9a8b6b749894ac6474b07ddeba569cb6d24166
Markus Ineichen <kusi.ineichen@gmail.com>
2025-11-16 21:24:01 +0100

    Add traits to derivation

--- examples/tick/src/lib.rs
+++ examples/tick/src/lib.rs
@@ -3,6 +3,7 @@ use std::num::NonZeroU64;
 
 pilatus::unstable_pub!(
     #[derive(Debug, Default, Deserialize, Serialize, PartialEq, impex::Impex)]
+    #[impex(derive(PartialEq, Clone))]
     #[serde(deny_unknown_fields)]
     struct GreeterParams {
         pub lang: GreeterLanguage,
@@ -22,6 +23,7 @@ impl impex::ImpexPrimitive for GreeterLanguage {}
 
 pilatus::unstable_pub!(
     #[derive(Serialize, Deserialize, Default, Debug, PartialEq, impex::Impex)]
+    #[impex(derive(PartialEq, Eq, Clone))]
     #[serde(default)]
     struct ManualTickParams {
         pub initial_count: u32,
@@ -30,6 +32,7 @@ pilatus::unstable_pub!(
 
 pilatus::unstable_pub!(
     #[derive(Debug, Clone, Deserialize, Serialize, PartialEq, impex::Impex)]
+    #[impex(derive(PartialEq, Eq, Clone))]
     #[serde(deny_unknown_fields)]
     struct TimerTickParams {
         pub milli_seconds_per_step: NonZeroU64,
